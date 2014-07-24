3263a5d3ddadfa573b9a2c0aced04812d902cef1
jgabry <jgabry@Jonahs-MacBook-Pro.local>
2014-07-24 19:42:42 -0400

    edited help files

--- R/meltMyTS.R
+++ R/meltMyTS.R
@@ -4,19 +4,19 @@
 #' \code{\link[QMSS]{ggMyTS}} (\pkg{QMSS}) to make plots of time series trends
 #' with \code{\link[ggplot2]{ggplot}}.
 #'
-#' @param mv.ts.object a multivariate time series object created with \code{\link[stats]{ts}}.
+#' @param mv.ts.object A multivariate time series object created with \code{\link[stats]{ts}}.
 #' @param time.var A character string naming the time variable.
-#' to either side of the cutpoint for which the estimate is calculated.
 #' @param keep.vars An optional character vector with names of variables to keep. 
-#' Variables not named in \code{keep.vars} will be dropped. If \code{keep.vars} is
-#' not specified then all variables in \code{mv.ts.object} will kept.
+#' Variables not named in \code{keep.vars} will be dropped. 
+#' If \code{keep.vars} is not specified then all variables in \code{mv.ts.object} 
+#' will be kept.
 #' @return A molten data frame.
 #' @author Jonah Gabry <jsg2201@@columbia.edu>. See \code{\link[reshape2]{melt}} 
 #' in (\pkg{reshape2}) for the author of the original \code{melt} function. 
 #' @seealso \code{\link[QMSS]{ggMyTS}}, \code{\link[reshape2]{melt.data.frame}} 
 #' @export
 #' @examples
-#' See examples in documentation for \code{\link[QMSS]{ggMyTS}}.
+#' See examples in documentation for ggMyTS.
 
 meltMyTS <- function(mv.ts.object, time.var, keep.vars){
   # mv.ts.object = a multivariate ts object
