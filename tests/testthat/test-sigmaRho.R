3ef2b47ee235282b36d051d5e4aa61e1e58ffabe
jgabry <jgabry@gmail.com>
2016-06-06 19:44:25 -0400

    also test sigmaRho for model="within"

--- tests/testthat/test-sigmaRho.R
+++ tests/testthat/test-sigmaRho.R
@@ -3,13 +3,27 @@ library(plm)
 context("sigmaRho")
 
 data("Produc")
-fit <- plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp,
-           data = Produc, index = c("state","year"), model = "random")
-sigmaRho(fit)
+fit_random <- plm(
+  log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp,
+  data = Produc,
+  index = c("state", "year"),
+  model = "random"
+)
+fit_within <- plm(
+  log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp,
+  data = Produc,
+  index = c("state", "year"),
+  model = "within"
+)
 
-test_that("sigmaRho returns expected result", {
-  a <- c(sigma_u = 0.0827, sigma_e = 0.0381, rho = 0.8246)
-  expect_equal(sigmaRho(fit), a, tol = 0.0001)
+test_that("sigmaRho returns expected result with model='random'", {
+  ans <- c(sigma_u = 0.0827, sigma_e = 0.0381, rho = 0.8246)
+  expect_equal(sigmaRho(fit_random), ans, tol = 0.0001)
+})
+
+test_that("sigmaRho returns expected result with model='within'", {
+  ans <- c(sigma_u = 0.0905, sigma_e = 0.0381, rho = 0.8494)
+  expect_equal(sigmaRho(fit_within), ans, tol = 0.0001)
 })
 
 test_that("sigmaRho prints to console", {
